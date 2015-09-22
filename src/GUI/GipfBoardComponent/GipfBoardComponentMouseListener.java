6eba0530912ed708e4d12f9523d6157e11727dbf
frans <fam.manders@student.maastrichtuniversity.nl>
2015-09-22 20:40:55 +0200

    Continued refactoring. Split GipfBoardComponentMouseListener into two classes. GipfBoardComponentMouseListener and HoverPositionUpdater

--- src/GUI/GipfBoardComponent/GipfBoardComponentMouseListener.java
+++ src/GUI/GipfBoardComponent/GipfBoardComponentMouseListener.java
@@ -1,6 +1,5 @@
 package GUI.GipfBoardComponent;
 
-import GUI.GipfBoardComponent.GipfBoardComponent;
 import GameLogic.Position;
 
 import java.awt.*;
@@ -15,7 +14,6 @@ import java.util.concurrent.TimeUnit;
  * Created by frans on 22-9-2015.
  */
 public class GipfBoardComponentMouseListener implements MouseListener {
-    private final int hoverUpdateIntervalMs = 100;                                  // The interval in ms of updating the position over which is being hovered
     GipfBoardComponent gipfBoardComponent;
     private Thread hoverThread;
 
@@ -47,7 +45,7 @@ public class GipfBoardComponentMouseListener implements MouseListener {
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        hoverThread = new Thread(new UpdateHoverPosition(gipfBoardComponent));
+        hoverThread = new Thread(new HoverPositionUpdater(gipfBoardComponent));
         hoverThread.start();
     }
 
@@ -57,57 +55,5 @@ public class GipfBoardComponentMouseListener implements MouseListener {
 
         gipfBoardComponent.currentHoverPosition = null;
         gipfBoardComponent.repaint();
-    }
-
-    /**
-     * This class contains code that is run in a separate thread and controls the updating of hover positions. It is ran in
-     * a different thread to make sure that the UI is not locked when the player hovers over positions
-     */
-    private class UpdateHoverPosition implements Runnable {
-        GipfBoardComponent gipfBoardComponent;
-        private Position previousPosition = null;
-
-        UpdateHoverPosition(GipfBoardComponent gipfBoardComponent) {
-            this.gipfBoardComponent = gipfBoardComponent;
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(hoverUpdateIntervalMs);
-                } catch (InterruptedException e) {
-                    // Interrupt the thread
-                    break;
-                }
-
-                Point mouseLocation = MouseInfo.getPointerInfo().getLocation();                             // Get the mouse position relative to the screen
-                Point componentPosition = gipfBoardComponent.getLocationOnScreen();                                            // Get the component position relative to the screen
-                mouseLocation.translate((int) -componentPosition.getX(), (int) -componentPosition.getY()); // Calculate the mouse position relative to the component
-
-                // Only update the position if the new position is different from the old position, and if the new
-                // position is actually located on the board
-                Position newHoverPosition = gipfBoardComponent.screenCoordinateToPosition((int) mouseLocation.getX(), (int) mouseLocation.getY());
-                if (newHoverPosition != previousPosition) {
-                    if (gipfBoardComponent.game.isPositionOnBoard(newHoverPosition)) {
-                        if (gipfBoardComponent.selectablePositions.contains(newHoverPosition)) {
-                            gipfBoardComponent.currentHoverPosition = gipfBoardComponent.screenCoordinateToPosition((int) mouseLocation.getX(), (int) mouseLocation.getY());
-                            previousPosition = gipfBoardComponent.currentHoverPosition;
-                            gipfBoardComponent.selectedMoveToPosition = null;
-                        } else if (gipfBoardComponent.selectedPosition != null && gipfBoardComponent.moveToPositions.contains(newHoverPosition)) {
-                            gipfBoardComponent.currentHoverPosition = gipfBoardComponent.screenCoordinateToPosition((int) mouseLocation.getX(), (int) mouseLocation.getY());
-                            gipfBoardComponent.selectedMoveToPosition = gipfBoardComponent.currentHoverPosition;
-                            previousPosition = gipfBoardComponent.currentHoverPosition;
-                        } else {
-                            gipfBoardComponent.currentHoverPosition = null;
-                        }
-                    } else {
-                        gipfBoardComponent.currentHoverPosition = null;
-                        gipfBoardComponent.selectedMoveToPosition = null;
-                    }
-                    gipfBoardComponent.repaint();
-                }
-            }
-        }
     }
 }
---
+++ src/GUI/GipfBoardComponent/HoverPositionUpdater.java
@@ -0,0 +1,62 @@
+package GUI.GipfBoardComponent;
+
+import GameLogic.Position;
+
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This class contains code that is run in a separate thread and controls the updating of hover positions. It is ran in
+ * a different thread to make sure that the UI is not locked when the player hovers over positions
+ * <p/>
+ * Created by frans on 22-9-2015.
+ */
+public class HoverPositionUpdater implements Runnable {
+    private final int hoverUpdateIntervalMs = 100;                                  // The interval in ms of updating the position over which is being hovered
+
+    GipfBoardComponent gipfBoardComponent;
+    private Position previousPosition = null;
+
+    HoverPositionUpdater(GipfBoardComponent gipfBoardComponent) {
+        this.gipfBoardComponent = gipfBoardComponent;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(hoverUpdateIntervalMs);
+            } catch (InterruptedException e) {
+                // Interrupt the thread
+                break;
+            }
+
+            Point mouseLocation = MouseInfo.getPointerInfo().getLocation();                             // Get the mouse position relative to the screen
+            Point componentPosition = gipfBoardComponent.getLocationOnScreen();                                            // Get the component position relative to the screen
+            mouseLocation.translate((int) -componentPosition.getX(), (int) -componentPosition.getY()); // Calculate the mouse position relative to the component
+
+            // Only update the position if the new position is different from the old position, and if the new
+            // position is actually located on the board
+            Position newHoverPosition = gipfBoardComponent.screenCoordinateToPosition((int) mouseLocation.getX(), (int) mouseLocation.getY());
+            if (newHoverPosition != previousPosition) {
+                if (gipfBoardComponent.game.isPositionOnBoard(newHoverPosition)) {
+                    if (gipfBoardComponent.selectablePositions.contains(newHoverPosition)) {
+                        gipfBoardComponent.currentHoverPosition = gipfBoardComponent.screenCoordinateToPosition((int) mouseLocation.getX(), (int) mouseLocation.getY());
+                        previousPosition = gipfBoardComponent.currentHoverPosition;
+                        gipfBoardComponent.selectedMoveToPosition = null;
+                    } else if (gipfBoardComponent.selectedPosition != null && gipfBoardComponent.moveToPositions.contains(newHoverPosition)) {
+                        gipfBoardComponent.currentHoverPosition = gipfBoardComponent.screenCoordinateToPosition((int) mouseLocation.getX(), (int) mouseLocation.getY());
+                        gipfBoardComponent.selectedMoveToPosition = gipfBoardComponent.currentHoverPosition;
+                        previousPosition = gipfBoardComponent.currentHoverPosition;
+                    } else {
+                        gipfBoardComponent.currentHoverPosition = null;
+                    }
+                } else {
+                    gipfBoardComponent.currentHoverPosition = null;
+                    gipfBoardComponent.selectedMoveToPosition = null;
+                }
+                gipfBoardComponent.repaint();
+            }
+        }
+    }
+}
