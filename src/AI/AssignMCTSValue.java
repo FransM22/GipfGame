e412d17ad5d66238790db9b682b9d32f6d19cce2
frans <fam.manders@student.maastrichtuniversity.nl>
2015-11-18 15:12:08 +0100

    Added MCTS player. Still needs some improvements

--- src/AI/AssignMCTSValue.java
+++ src/AI/AssignMCTSValue.java
@@ -1,5 +1,7 @@
 package AI;
 
+import GameLogic.Game.BasicGame;
+import GameLogic.Game.Game;
 import GameLogic.GipfBoardState;
 
 import java.util.function.Function;
@@ -10,6 +12,20 @@ import java.util.function.Function;
 public class AssignMCTSValue implements Function<GipfBoardState, Double> {
     @Override
     public Double apply(GipfBoardState gipfBoardState) {
-        return Math.random();    // TODO update with real value
+        double var4 = 0;
+
+        for (int number_simulations_left = 100; number_simulations_left > 0; number_simulations_left--) {
+            // Create a temporary game
+            Game game = new BasicGame();
+            game.loadState(gipfBoardState);
+
+            // From here a move can be applied
+            // any move from
+            // game.getAllowedMoves();
+            // is allowed
+        }
+
+
+        return  var4;    // TODO update with real value
     }
 }
